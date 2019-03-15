d2d7489a67d9dcbaef38f09a285bf23d7f4764ea
yekta <ngyekta@gmail.com>
2019-03-16 02:29:31 +0300

    ChatListItem component created

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,23 @@
     <Navbar></Navbar>
     <!-- NAVBAR END -->
     <!-- ENTER MESSAGE SECTION -->
-    <div class="section py-4 py-md-5" id="enter-message">
+    <div class="section py-4 py-md-5" id="enter-message-section">
       <div class="container py-4 py-md-5">
-        <div class="row align-items-center d-flex justify-content-around px-4">
+        <div class="row align-items-center d-flex justify-content-around">
           <div class="col-12 col-md-10">
             <form>
               <div class="input-group">
                 <input
                   type="text"
-                  class="form-control form-control-lg rounded-100 text-primary bg-transparent border-primary px-4 col-12 col-md-9"
+                  class="form-control form-control-lg rounded-100 text-primary bg-transparent border-primary px-4 col-12 col-md-9 ml-0 ml-md-3 ml-lg-4"
                   id="messageInput"
                   placeholder="write a message"
                 >
                 <span class="input-group-btn col-12 col-md-3 mt-3 mt-md-0">
-                  <button type="submit" class="btn btn-lg btn-primary w-100 font-weight-bold mx-0">send</button>
+                  <button
+                    type="submit"
+                    class="btn btn-lg btn-primary w-100 font-weight-bold mx-0"
+                  >send</button>
                 </span>
               </div>
             </form>
@@ -26,13 +29,32 @@
       </div>
     </div>
     <!-- ENTER MESSAGE SECTION END -->
-    <h1>{{ msg }}</h1>
+    <!-- CHAT SECTION -->
+    <div class="section" id="chat-section">
+      <div class="container">
+        <div class="row align-items-center d-flex justify-content-between">
+          <div class="col-12 col-md-10 col-lg-10 mx-auto">
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+            <ChatListItem></ChatListItem>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- CHAT SECTION END -->
   </div>
 </template>
 
 <script>
 import Vue from "vue";
 import Navbar from "./components/Navbar.vue";
+import ChatListItem from "./components/ChatListItem.vue";
 
 export default Vue.extend({
   name: "app",
@@ -42,7 +64,8 @@ export default Vue.extend({
     };
   },
   components: {
-    Navbar
+    Navbar,
+    ChatListItem
   }
 });
 </script>
---
+++ src/components/ChatListItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="row align-items-center d-flex justify-content-around">
+    <div class="col">
+      <img class="w-100" src="../assets/img/placeholder-monkey.svg">
+    </div>
+    <div class="col-7 col-md-8 col-lg-9 bg-secondary py-1 py-md-3 py-lg-4 speech-bubble mr-4 mr-md-5">
+      <div class="row align-items-center align-middle d-flex justify-content-between py-2">
+        <h3 class="text-left text-light font-weight-light ml-4">hello world</h3>
+        <h5 class="text-left text-light font-weight-light mr-4">02:16 / 03.16.2019</h5>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+export default Vue.extend({
+  name: "ChatListItem"
+});
+</script>
