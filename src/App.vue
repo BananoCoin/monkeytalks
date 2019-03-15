3c7bc7bd36bf0922cfda4cc5304c235e82d6b2f1
yekta <ngyekta@gmail.com>
2019-03-15 22:52:11 +0300

    Some styling and navbar added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
-  <div id="app">
-    <img src="../assets/img/banano.svg">
-    <h1>\{{ msg }}</h1>
+  <div id="app" class="bg-dark">
+    <!-- NAVBAR -->
+    <div class="container-fluid z-top">
+        <nav>
+            <div class="container">
+                <div class="row d-flex justify-content-between py-3">
+                    <a href="./">
+                        <img src="../assets/img/monkeytalks-logo.svg">
+                    </a>
+                    <button class="btn btn-primary btn-lg px-4 px-md-5 font-weight-bold">faucet</button>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <!-- NAVBAR END -->
+
+    <h1>{{ msg }}</h1>
   </div>
 </template>
 
@@ -12,7 +26,7 @@ export default Vue.extend({
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: ''
     }
   },
 })
