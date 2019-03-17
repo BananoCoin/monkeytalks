72705aa18a0cd45caeb188cf5a009963300336a7
yekta <ngyekta@gmail.com>
2019-03-18 02:59:20 +0300

    Logo narrow added

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,7 +4,8 @@
       <div class="container">
         <div class="row d-flex justify-content-between py-3">
           <a href="./">
-            <img src="../assets/img/monkeytalks-logo.svg">
+            <img class="grow-3 d-none d-md-block" src="../assets/img/monkeytalks-logo.svg">
+            <img class="grow-3 d-block d-md-none" src="../assets/img/monkeytalks-logo-narrow.svg">
           </a>
           <button @click = "toggleFaucet" v-bind:class="[$store.state.showHide ? ['btn-secondary', 'text-light', 'glow-purple'] : ['btn-primary', 'text-dark', 'glow-green'] ]"
             class="btn btn-lg px-4 px-md-5"
