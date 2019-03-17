5c181e7ddd57fcb5a2cbd475a06ef3880c35ff33
yekta <ngyekta@gmail.com>
2019-03-17 23:43:52 +0300

    Popups added

--- src/components/ChatListItem.vue
+++ src/components/ChatListItem.vue
@@ -1,9 +1,28 @@
 <template>
   <div class="row align-items-center d-flex justify-content-around my-2 my-md-3">
-    <div class="col">
-      <img class="w-100" src="../assets/img/placeholder-monkey.svg">
+    <div class="col z-index-100">
+      <popper
+        :options="{
+          placement: 'left',
+          modifiers: { offset: { offset: '0,10px' } },   
+        }"
+        :transition = "fade"
+      >
+        <div class="popper bg-secondary p-3 speech-bubble-two">
+          <h3 class="text-left text-primary font-weight-light">Messages: 12</h3>
+          <p class="text-left text-light font-weight-light">ban_1yekta1xn94qdnbmmj
+            <br>1tqg76zk3apcfd31pjmuy6
+            <br>d879e3mr469a4o4sdhd4
+          </p>
+        </div>
+        <button slot="reference" class="btn-primary bg-transparent border-none rounded-100 px-0">
+          <img class="w-100 grow" src="../assets/img/placeholder-monkey.svg">
+        </button>
+      </popper>
     </div>
-    <div class="col-7 col-md-8 col-lg-9 bg-secondary py-1 py-md-3 py-lg-4 speech-bubble mr-4 mr-md-5">
+    <div
+      class="col-7 col-md-8 col-lg-9 bg-secondary py-1 py-md-3 py-lg-4 speech-bubble mr-4 mr-md-5"
+    >
       <div class="row align-items-center align-middle d-flex justify-content-between py-2">
         <h3 class="text-left text-light font-weight-light ml-4">{{ message.content }}</h3>
         <h5 class="text-left text-light font-weight-light mx-4">{{ message.date }}</h5>
@@ -14,10 +33,15 @@
 
 <script>
 import Vue from "vue";
+import Popper from "vue-popperjs";
+import "vue-popperjs/dist/vue-popper.css";
 export default Vue.extend({
   name: "ChatListItem",
   props: {
-    message : Object
+    message: Object
+  },
+  components: {
+    popper: Popper
   }
 });
 </script>
