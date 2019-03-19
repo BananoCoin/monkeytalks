92762fe1697bf2dff2ac1d12ca534c61fc22b8a8
yekta <ngyekta@gmail.com>
2019-03-19 19:51:31 +0300

    Text and qr added to SendCard

--- src/components/SendCardSection.vue
+++ src/components/SendCardSection.vue
@@ -3,31 +3,16 @@
     <div class="container px-0 px-md-4">
       <div class="container bg-secondary py-5 rounded-1 col-12 col-md-10">
         <div class="row align-items-center d-flex justify-content-around">
-          <div class="col-12">
-            <h3
-              class="text-center text-light font-weight-bold"
-            >Please enter your Banano address below.</h3>
-            <h5
-              class="text-center text-light font-weight-light"
-            >We will send you 0.1% of the faucet balance.</h5>
+          <div class="col-12 col-md-1">
+               <qriously value="Hello World!" :size="200" />
           </div>
-        </div>
-        <div class="row align-items-center d-flex justify-content-around py-3">
-          <div class="col-11 col-md-10 col-lg-8">
-            <form>
-              <div class="input-group">
-                <input
-                  type="text"
-                  class="font-weight-bold textfield-light form-control form-control-lg rounded-100 text-light bg-transparent border-light border-2 mt-3 px-4 px-lg-5"
-                  id="addressInput"
-                  placeholder="ban_address"
-                >
-              </div>
-              <button
-                type="submit"
-                class="btn btn-lg btn-light btn-block mt-3 mx-auto col-12 col-md-8 col-lg-6 text-secondary glow-pink"
-              >Send Me Some Banano</button>
-            </form>
+          <div class="col-12 col-md-5">
+               <h4 class="text-center text-md-left text-light font-weight-light">Your Message:</h4>
+               <h2 class="text-center text-md-left text-light font-weight-extrabold">Hello World</h2>
+               <br>
+               <h4 class="text-center text-md-left text-light font-weight-light">
+                    Scan the QR code with <span class="font-weight-extrabold h3">Kalium</span> and send <span class="font-weight-extrabold h3">10.12 Banano</span> to publish the message.
+               </h4>
           </div>
         </div>
       </div>
@@ -37,8 +22,10 @@
 
 <script>
 import Vue from "vue";
-import posed from 'vue-pose';
+import posed from "vue-pose";
+import VueQriously from 'vue-qriously'
+Vue.use(VueQriously)
 export default Vue.extend({
-  name: "SendCardSection",
+  name: "SendCardSection"
 });
 </script>
