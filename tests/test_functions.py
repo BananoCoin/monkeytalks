dea97084a176ffd929688b77e63479e05f3c6a65
bbedward <bbedward@gmail.com>
2019-03-24 14:51:59 -0400

    Add server-side address validation class

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,6 +1,7 @@
 import simplejson as json
 
 from app.util.nanote import Nanote
+from app.util.validations import Validations
 
 class TestAPI:
     def test_messages_api(self, testapp):
@@ -22,3 +23,20 @@ class TestUtil:
     def test_checksum_validation(self):
         assert(Nanote.validate_message('1473211653') == False)
         assert(Nanote.validate_message('3085200816947056507') == True)
+
+    def test_address_validations(self):
+        raw_string = 'abbcddfasdban_1ph8tfwan1jd91pcettzn8rg448pooin1tz9juhsq1wbhsijebgcho411kum242134143'
+        extracted = Validations.get_banano_address(raw_string)
+        assert(extracted == 'ban_1ph8tfwan1jd91pcettzn8rg448pooin1tz9juhsq1wbhsijebgcho411kum')
+        # Test valid address
+        assert(Validations.validate_address(extracted) == True)
+        # Test invalid character (l)
+        assert(Validations.validate_address('ban_1ph8tfwan1jd91pcettzn8rg448pooin1tz9juhsq1wbhsijebgcho411kul') == False)
+        # Test bad checksum
+        assert(Validations.validate_address('ban_1ph8tfwan1jd91pcettzn8rg448pooin1tz9juhsq1wbhsijebgcho411kuk') == False)
+        # Test wrong prefix
+        assert(Validations.validate_address('xrb_1ph8tfwan1jd91pcettzn8rg448pooin1tz9juhsq1wbhsijebgcho411kum') == False)
+        # Test length (too long)
+        assert(Validations.validate_address('ban_1ph8tfwan1jd91pcettzn8rg448pooin1tz9juhsq1wbhsijebgcho411kumk') == False)
+        # Test length (too short)
+        assert(Validations.validate_address('ban_1ph8tfwan1jd91pcettzn8rg448pooin1tz9juhsq1wbhsijebgcho411ku') == False)
