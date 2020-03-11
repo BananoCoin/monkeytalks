ea565136e7471fa98ce6fd22b0825762053b274c
yekta <ngyekta@gmail.com>
2020-03-12 00:29:15 +0300

    Border change

--- src/components/ChatListItemAdvanced.vue
+++ src/components/ChatListItemAdvanced.vue
@@ -25,7 +25,7 @@
         >{{ formatDate(message.date) }}</h6>
       </div>
       <div
-        v-bind:class="[message.premium ? ['bg-black-10', 'border-black-10'] : ['bg-black-15', 'border-black-05'] ]"
+        v-bind:class="[message.premium ? ['bg-black-10', 'border-black-05'] : ['bg-black-15', 'border-black-10'] ]"
         class="row align-items-center d-flex justify-content-center px-3 px-md-4 mx-0 py-1 my-2 rounded-2"
       >
         <a
