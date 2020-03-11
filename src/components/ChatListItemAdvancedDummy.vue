40e9a012b66fda81be9197f9edf03383a8f56c0f
yekta <ngyekta@gmail.com>
2020-03-11 21:16:20 +0300

    Trying a style

--- src/components/ChatListItemAdvancedDummy.vue
+++ src/components/ChatListItemAdvancedDummy.vue
@@ -3,20 +3,20 @@
     <div class="col-10 py-2 py-md-3 px-4 bg-secondary speech-bubble-purple opacity-animation-2">
       <div class="row align-items-center align-middle d-flex justify-content-between py-2">
         <h3
-          class="text-left ml-4 font-weight-bold text-light bg-light rounded-100 px-3 my-2 opacity-animation"
+          class="text-left font-weight-bold text-light bg-light rounded-100 px-3 my-2 opacity-animation"
         >hello world</h3>
         <h5
-          class="text-left mx-4 font-weight-light text-light bg-light rounded-100 px-3 mx-2 opacity-animation"
+          class="text-left font-weight-light text-light bg-light rounded-100 px-3 mx-2 opacity-animation"
         >22:15 / Thu, 16 Mar 2019</h5>
       </div>
       <div
-        class="row align-items-center d-flex justify-content-center px-3 px-md-4 mx-0 py-1 my-2 rounded-2 bg-black-15"
+        class="row align-items-center d-flex justify-content-center mx-md-2 px-3 px-md-4 py-3 my-2 rounded-2 bg-black-15"
       >
         <h6
           class="text-center font-weight-light break-word text-transparent tk-ibm-plex-mono text-small"
         >549536066067248428368798158059 raw</h6>
       </div>
-      <div class="row align-items-center d-flex justify-content-center py-1 px-3 px-md-4">
+      <div class="row align-items-center d-flex justify-content-center px-3 px-md-4 py-1">
         <h6
           class="tk-ibm-plex-mono text-center text-light font-weight-light bg-light break-word rounded-100 px-3 my-2 opacity-animation"
         >ban_3h9ykbjs5aor438byaopjekjcwnxixfhmuzsqepsoifxr4qnrfwmn1t6qfh3</h6>
