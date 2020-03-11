6c238cb853af610bff3affcf7f4ea703d6595777
yekta <ngyekta@gmail.com>
2020-03-11 21:29:49 +0300

    More small changes

--- src/components/ChatListItemAdvancedDummy.vue
+++ src/components/ChatListItemAdvancedDummy.vue
@@ -3,10 +3,10 @@
     <div class="col-10 py-2 py-md-3 px-4 bg-secondary speech-bubble-purple opacity-animation-2">
       <div class="row align-items-center align-middle d-flex justify-content-between py-2">
         <h3
-          class="text-left font-weight-bold mx-3 text-light bg-light rounded-100 px-3 my-2 opacity-animation"
+          class="text-left font-weight-bold mx-3 mx-md-4 text-light bg-light rounded-100 px-3 my-2 opacity-animation"
         >hello world</h3>
         <h5
-          class="text-left font-weight-light text-light bg-light rounded-100 px-3 mx-2 opacity-animation"
+          class="text-left font-weight-light mx-3 mx-md-4 text-light bg-light rounded-100 px-3 mx-2 opacity-animation"
         >22:15 / Thu, 16 Mar 2019</h5>
       </div>
       <div
